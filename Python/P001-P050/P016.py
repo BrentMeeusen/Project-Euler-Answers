a18f8f25964f8f9055582f0578b62b06941031d9
BrentMeeusen <brentm@live.nl>
2025-11-14 23:17:20 +0100

    Update code to match style of previous Python files

--- Python/P001-P050/P016.py
+++ Python/P001-P050/P016.py
@@ -1,18 +1,19 @@
 from Python.timer import timer
 
 
+def add(a: str, b: str) -> str:
+    ab_sum = ""
+    carry = 0
+    for i in range(len(a)):
+        col_sum = int(a[-i - 1]) + int(b[-i - 1]) + carry
+        ab_sum = str(col_sum % 10) + ab_sum
+        carry = col_sum // 10
+
+    return ("" if carry == 0 else str(carry)) + ab_sum
+
+
 @timer
 def p016(power: int = 5) -> int:
-    def add(a: str, b: str) -> str:
-        ab_sum = ""
-        carry = 0
-        for j in range(len(a)):
-            col_sum = int(a[-j - 1]) + int(b[-j - 1]) + carry
-            ab_sum = str(col_sum % 10) + ab_sum
-            carry = col_sum // 10
-
-        return ("" if carry == 0 else str(carry)) + ab_sum
-
     current_num = "1"
 
     for i in range(power):
