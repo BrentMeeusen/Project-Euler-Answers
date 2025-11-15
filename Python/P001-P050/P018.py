9456a1623c3b87ed2430567f66dbcf294c7e21a4
BrentMeeusen <brentm@live.nl>
2025-11-15 01:21:00 +0100

    Add P018

---
+++ Python/P001-P050/P018.py
@@ -0,0 +1,48 @@
+import re
+
+from Python.timer import timer
+
+pyramid_small = """3
+7 4
+2 4 6
+8 5 9 3"""
+
+pyramid_big = """75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
+
+
+def parse_pyramid(pyramid: str) -> list[list[int]]:
+    return [list(map(int, line.split(" "))) for line in re.split(r"[\r\n]+", pyramid)]
+
+
+@timer
+def p018(pyramid: str) -> int:
+    pyramid = parse_pyramid(pyramid)
+    optimal_pyramid = [list() for _ in range(len(pyramid))]
+    optimal_pyramid[-1] = pyramid[-1]
+
+    for row_index in range(1, len(pyramid)):
+        reversed_row_index = -row_index - 1
+
+        for col_index in range(len(pyramid[reversed_row_index])):
+            max_child = max(optimal_pyramid[reversed_row_index + 1][col_index],
+                            optimal_pyramid[reversed_row_index + 1][col_index + 1])
+            optimal_pyramid[reversed_row_index].append(pyramid[reversed_row_index][col_index] + max_child)
+
+    return optimal_pyramid[0][0]
+
+
+p018(pyramid_big)
